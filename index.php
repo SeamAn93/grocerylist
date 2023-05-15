c4480b399818e13415653693470ac5e916f84529
SeamAn93 <andersen-s93@hotmail.com>
2023-05-15 17:05:17 +0200

    Setup of projekt:
    - files
    - sass
    - watcher
    - bootstrap
    - gitignore

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    <title>Grocerylist</title>
+    <meta name="robots" content="All">
+    <meta name="author" content="Søren Andersen">
+    <meta name="copyright" content="© Søren Andersen, 2023">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<section class="grocerylist">
+    <div class="grocerylis__headline">
+        <h1 class="grocerylist__headline__text">Indkøbsliste</h1>
+    </div>
+</section>
+
+<script src="js/mainscript.js"></script>
+</body>
+</html>
